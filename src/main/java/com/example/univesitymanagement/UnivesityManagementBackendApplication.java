baf97aa8f0603fb522b10855dfa3d87f43666d73
Ons Janeb <onesjaneb@gmail.com>
2024-03-17 16:42:29 +0100

    first commit

---
+++ src/main/java/com/example/univesitymanagement/UnivesityManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.univesitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivesityManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivesityManagementBackendApplication.class, args);
+	}
+
+}
