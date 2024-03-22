94dcfc4e605c07b33f58d2db3018c66abf482678
Ons Janeb <onesjaneb@gmail.com>
2024-03-22 01:51:11 +0100

    create student Repository

---
+++ src/main/java/com/example/univesitymanagement/repository/EtudiantRepository.java
@@ -0,0 +1,8 @@
+package com.example.univesitymanagement.repository;
+
+import com.example.univesitymanagement.entity.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+
+}
